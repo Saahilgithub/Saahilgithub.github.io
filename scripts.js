ea198fe1e60a374688c4513335d455fe2822b6ba
Saahilgithub <137374675+Saahilgithub@users.noreply.github.com>
2023-08-20 11:53:00 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,2 @@
-// Add smooth scrolling to navigation links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', function (event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: 'smooth'
-        });
-    });
-});
+
 
